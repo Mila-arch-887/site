4ef75d7c206661afb7736b60cd73781c82b82a2d
Mila-arch-887 <rhillaryoliveira345@gmail.com>
2026-04-02 15:49:39 -0300

    Update gera_qr_visual.py

--- gera_qr_visual.py
+++ gera_qr_visual.py
@@ -1,10 +1,10 @@
 import qrcode
-from PIL import Image
 
 URL_SITE = "https://site-production-6665.up.railway.app"
 
 # Gera o QR code
 img = qrcode.make(URL_SITE)
 
-# Mostra a imagem direto
+# Salva e mostra a imagem automaticamente
+img.save("qrcode.png")
 img.show()
